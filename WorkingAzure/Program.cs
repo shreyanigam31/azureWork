48a0c091bf962bb0e0145d0f90513afe5b169736
Shreya <Shreya1.Nigam@lntInfotech.com>
2020-07-29 10:39:06 +0530

    azure

---
+++ WorkingAzure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hii");
+            Console.ReadKey();
+        }
+    }
+}
